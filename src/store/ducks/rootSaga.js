f6ba4326406c57027701406e903f1bc99c868e42
Arlen Vasconcelos <arlen.vasconcelos@gmail.com>
2020-07-07 16:28:51 -0300

    remove warnings

--- src/store/ducks/rootSaga.js
+++ src/store/ducks/rootSaga.js
@@ -1,12 +1,9 @@
-import { fork, all, takeLatest } from "redux-saga/effects";
+import { fork, all } from "redux-saga/effects";
 
-// import { Types as StoreTypes } from "./store/store";
 import { loadStore } from "./store/sagas";
 import { loadProducts } from "./products/sagas";
 import { loadClients } from "./clients/sagas";
 
 export default function* rootSaga() {
-  // return yield all([takeLatest(StoreTypes.LOAD_REQUEST, loadStore)]);
-
   return yield all([fork(loadStore), fork(loadProducts), fork(loadClients)]);
 }
